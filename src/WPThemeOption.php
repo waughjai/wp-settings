2b5dbde8a8f238c4df9113f2bd30b712a08e2401
Jaimeson Waugh <jaimeson@4cesi.com>
2018-12-13 16:25:54 -0800

    Add checkbox input type

--- src/WPThemeOption.php
+++ src/WPThemeOption.php
@@ -4,14 +4,16 @@ declare( strict_types = 1 );
 namespace WaughJ\WPThemeOption
 {
 	use function WaughJ\TestHashItem\TestHashItemExists;
+	use WaughJ\VerifiedArgumentsSameType\VerifiedArgumentsSameType;
 
 	class WPThemeOption
 	{
-		public function __construct( WPThemeOptionsSection $section, string $slug, string $name )
+		public function __construct( WPThemeOptionsSection $section, string $slug, string $name, array $other_attributes = [] )
 		{
 			$this->section = $section;
 			$this->slug = $slug;
 			$this->name = __( $name, 'textdomain' );
+			$this->other_attributes = new VerifiedArgumentsSameType( $other_attributes, self::DEFAULT_ATTRIBUTES );
 			add_action( 'admin_init', [ $this, 'register' ] );
 		}
 
@@ -30,7 +32,21 @@ namespace WaughJ\WPThemeOption
 
 		public function render() : void
 		{
-			?><input type="text" id="<?= $this->slug; ?>" name="<?= $this->section->getPage()->getOptionsGroup(); ?>[<?= $this->slug; ?>]" placeholder="<?= $this->name; ?>" value="<?= $this->getOptionValue(); ?>" /><?php
+			switch ( $this->other_attributes->get( 'input_type' ) )
+			{
+				case( 'checkbox' ):
+				{
+					$checked_text = ( $this->getOptionValue() ) ? ' checked="checked"' : '';
+					?><input type="checkbox" id="<?= $this->slug; ?>" name="<?= $this->section->getPage()->getOptionsGroup(); ?>[<?= $this->slug; ?>]"<?= $checked_text; ?> /><?php
+				}
+				break;
+
+				default:
+				{
+					?><input type="text" id="<?= $this->slug; ?>" name="<?= $this->section->getPage()->getOptionsGroup(); ?>[<?= $this->slug; ?>]" placeholder="<?= $this->name; ?>" value="<?= $this->getOptionValue(); ?>" /><?php
+				}
+				break;
+			}
 		}
 
 		public function getOptionValue() : string
@@ -41,9 +57,15 @@ namespace WaughJ\WPThemeOption
 				: ( string )( $options );
 		}
 
+		const DEFAULT_ATTRIBUTES =
+		[
+			'input_type' => 'text'
+		];
+
 		private $page;
 		private $section;
 		private $slug;
 		private $name;
+		private $other_attributes;
 	}
 }
--- tests/WPThemeOptionTest.php
+++ tests/WPThemeOptionTest.php
@@ -18,4 +18,14 @@ class WPThemeOptionTest extends TestCase
 		$option->render();
 		$this->assertEquals( '<input type="text" id="main_css" name="theme_design_options[main_css]" placeholder="Main CSS" value="" />', ob_get_clean() );
 	}
+
+	public function testCheckbox()
+	{
+		$page = new WPThemeOptionsPage( 'design', 'Design' );
+		$section = new WPThemeOptionsSection( $page, 'main_scripts', 'Main Scripts' );
+		$option = new WPThemeOption( $section, 'show_header', 'Show Header?', [ 'input_type' => 'checkbox' ] );
+		ob_start();
+		$option->render();
+		$this->assertEquals( '<input type="checkbox" id="show_header" name="theme_design_options[show_header]" />', ob_get_clean() );
+	}
 }
